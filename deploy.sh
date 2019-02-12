97d1ceed217a362ee1201b5599200e9b359e8633
blueshu <jsgu@aceinna.com>
2019-02-12 09:31:35 +0800

    update deploy message

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,19 @@
-cd /home/www/forumAceinna/
+cd /home/www/ans-forum/
 
 echo "Pulling from Master" 
 
 git pull origin master
 
 echo "Pulled successfully from master"
+
+npm run stop
+
+echo "stop"
+
+npm run compareBuild
+
+echo "builded"
+
+nohup npm start &
+
+echo "start"
